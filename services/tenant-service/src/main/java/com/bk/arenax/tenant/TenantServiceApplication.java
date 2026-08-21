81130eb5cba7bbfdd8bc12b3c0d9731fc78b2a49
LDKhangg <lekhangmc12@gmail.com>
2026-08-21 16:18:13 +0700

    refactor: remove unused openfeign bootstrap

--- services/tenant-service/src/main/java/com/bk/arenax/tenant/TenantServiceApplication.java
+++ services/tenant-service/src/main/java/com/bk/arenax/tenant/TenantServiceApplication.java
@@ -2,10 +2,8 @@ package com.bk.arenax.tenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TenantServiceApplication {
 
     public static void main(String[] args) {
